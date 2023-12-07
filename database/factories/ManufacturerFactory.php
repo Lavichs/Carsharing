2efee25d817f5493918117fd2906a1f4d900efd3
Ilya Shurygin <Ilya.shurygin-1@yandex.ru>
2023-12-07 22:20:54 +0500

    feat(ManufacturerFactory.php): create factory

---
+++ database/factories/ManufacturerFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Manufacturer>
+ */
+class ManufacturerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->company,
+            'address' => $this->faker->address,
+            'MSNR' => random_int(1000000000000, 9999999999999) . "",
+        ];
+    }
+}
