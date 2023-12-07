04f33ecc30b0b32bd0d3488d5a0e5a4ec2a7364d
Ilya Shurygin <Ilya.shurygin-1@yandex.ru>
2023-12-08 00:53:41 +0500

    fix(app\Enums\...): add randomValue() to some Enums
    * add method to UserStatusesEnum
    * add method to RentStatusesEnum

--- app/Enums/RentStatusesEnum.php
+++ app/Enums/RentStatusesEnum.php
@@ -7,4 +7,11 @@ enum RentStatusesEnum: int
 	case Close = 0;
 	case Open = 1;
 	case ClosedWithIncident = 2;
+
+	public static function randomValue(): string
+    {
+        $arr = array_column(self::cases(), 'value');
+
+        return $arr[array_rand($arr)];
+    }
 }
--- app/Enums/UserStatusesEnum.php
+++ app/Enums/UserStatusesEnum.php
@@ -6,4 +6,11 @@ enum UserStatusesEnum: int
 {
 	case Ordinary = 0;
 	case TempBanned = 1;
+
+	public static function randomValue(): string
+    {
+        $arr = array_column(self::cases(), 'value');
+
+        return $arr[array_rand($arr)];
+    }
 }
