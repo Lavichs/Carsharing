ccf7cbd016fbd24c60937b02901d81bd56322194
Ilya Shurygin <Ilya.shurygin-1@yandex.ru>
2023-12-07 22:53:32 +0500

    feat(CarStatusesEnum.php): add method randomValue

--- app/Enums/CarStatusesEnum.php
+++ app/Enums/CarStatusesEnum.php
@@ -4,7 +4,14 @@ namespace App\Enums;
 
 enum CarStatusesEnum: int
 {
-	case Wait = 0,
-	case Move = 1,
-	case Repair = 2,
+	case Wait = 0;
+	case Move = 1;
+	case Repair = 2;
+
+	public static function randomValue(): string
+    {
+        $arr = array_column(self::cases(), 'value');
+
+        return $arr[array_rand($arr)];
+    }
 }
