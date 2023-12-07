a3c7c9ad037f60799b5046b47aa0b2988c0a5ee7
Ilya Shurygin <Ilya.shurygin-1@yandex.ru>
2023-12-07 11:38:25 +0500

    docs(Manufacturer.php): add comment to field MSNR

--- database/migrations/2023_12_07_061138_create_manufacturers_table.php
+++ database/migrations/2023_12_07_061138_create_manufacturers_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title', 50);
             $table->string('address', 100);
-            $table->string('MSNR', 13);
+            $table->string('MSNR', 13);     // The Main State Registration Number (Основной Государственный Регистрационный Номер)
             $table->timestamps();
             $table->softDeletes();
         });
